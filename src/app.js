0a70573dc23427fc11959befedc727440170e0e1
anmol <anmol333111@gmail.com>
2025-09-13 22:20:19 +0530

    Whitelisted frontend domain

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://xeno-mini-crm-frontend-rust.vercel.app"],
     credentials: true,
   })
 );
